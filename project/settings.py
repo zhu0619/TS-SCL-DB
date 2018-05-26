958930c06bc0117729ec476283d0d0371fc60213
LuZhu <zhu.lu@hotmail.com>
2018-05-26 03:03:59 +0200

    back to debug mode

--- project/settings.py
+++ project/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
